85bed13b4b37dfdfe5a9f1f9d1c32b1552ade34c
Flavio-Quesada-dev <flavioquesadam@gmail.com>
2026-03-30 15:50:02 -0300

    Create app.js
    Agrego estructura inicial para Web-Dev y primer script JS

---
+++ Web-Dev/app.js
@@ -0,0 +1,7 @@
+// 🌐 Primer paso en el Desarrollo Web
+// Objetivo: Aprender JavaScript para trabajar en el Frontend de Shopify
+
+const mensaje = "Hola Shopify, practicando JS para el futuro";
+
+console.log(mensaje);
+console.log("Estado del sistema: Operativo 🚀");
